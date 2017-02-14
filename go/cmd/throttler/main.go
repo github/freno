3742db8cd918c49b47134c8fcd7b189fb98ac299
miguelff <miguelff@github.com>
2017-02-14 11:23:48 +0100

    Change project layout to have 1 binary

--- go/cmd/throttler/main.go
+++ go/cmd/throttler/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	gohttp "net/http"
+
+	"github.com/github/throttler/go/http"
 	"github.com/outbrain/golib/log"
 )
 
 func main() {
-	log.Infof("Starting")
+	server := flag.String("server", "", "spawn the HTTP API server")
+	port := flag.Int("port", 8080, "the port number, defaults to 8080")
+	flag.Parse()
+
+	if *server != "" {
+		mainServer(*port)
+	}
+
+}
+
+func mainServer(port int) {
+	api := new(http.APIImpl)
+	router := http.ConfigureRoutes(api)
+	log.Infof(fmt.Sprintf("Starting server in port %d", port))
+	gohttp.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
