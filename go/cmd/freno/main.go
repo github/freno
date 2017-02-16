340cfa7290fd85c2c16ee7cec003e2f3bd3a07f4
miguelff <miguelff@github.com>
2017-02-16 18:12:32 +0100

    Load some settings from config

--- go/cmd/freno/main.go
+++ go/cmd/freno/main.go
@@ -19,19 +19,53 @@ func main() {
 	}
 	log.Infof("starting freno %s", AppVersion)
 
-	server := flag.String("server", "", "spawn the HTTP API server")
-	port := flag.Int("port", config.Parameters().ListenPort, fmt.Sprintf("the port number, defaults to %d", config.Parameters().ListenPort))
+	configFile := flag.String("config", "", "config file name")
+	http := flag.Bool("http", false, "spawn the HTTP API server")
+	help := flag.Bool("help", false, "show the help")
 	flag.Parse()
 
-	if *server != "" {
-		mainServer(*port)
-	}
+	loadConfiguration(*configFile)
 
+	flag.Parse()
+
+	switch {
+	case *http:
+		httpServe()
+	case *help:
+		printHelp()
+	default:
+		printUsage()
+	}
 }
 
-func mainServer(port int) {
+func loadConfiguration(configFile string) {
+	var err error
+	if configFile != "" {
+		err = config.Instance().Read(configFile)
+	} else {
+		err = config.Instance().Read("/etc/freno.conf.json", "conf/freno.conf.json", "freno.conf.json")
+	}
+
+	if err != nil {
+		panic("Error reading configuration, please check the logs. Error was: " + err.Error())
+	}
+}
+
+func httpServe() {
 	api := http.NewAPIImpl()
 	router := http.ConfigureRoutes(api)
+	port := config.Settings().ListenPort
 	log.Infof(fmt.Sprintf("Starting server in port %d", port))
 	gohttp.ListenAndServe(fmt.Sprintf(":%d", port), router)
+}
+
+func printHelp() {
+	panic("not yet implemented")
+}
+
+func printUsage() {
+	fmt.Println(`Usage: freno [OPTIONS]
+
+	For more help options use: freno help.
+	`)
 }
