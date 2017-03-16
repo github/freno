54bd6ddf048a5f32e84fbeddc58d85d127c7001b
miguelff <miguelff@github.com>
2017-03-16 10:58:32 +0100

    `ratecounter.FromPool` gets `ExpvarRateCounter` objects

--- go/ratecounter/pool.go
+++ go/ratecounter/pool.go
@@ -1,6 +1,7 @@
 package ratecounter
 
 import (
+	"expvar"
 	"fmt"
 	"sync"
 	"time"
@@ -8,35 +9,61 @@ import (
 	rc "github.com/paulbellamy/ratecounter"
 )
 
+// ExpvarRateCounter groups together a RateCounter and an expvarInt
+// to export counters rated at a certain interval to expvar
+type ExpvarRateCounter struct {
+	interval    time.Duration
+	rateCounter *rc.RateCounter
+	expvarInt   *expvar.Int
+}
+
+func newExpvarRateCounter(name string, interval time.Duration) *ExpvarRateCounter {
+	return &ExpvarRateCounter{
+		interval:    interval,
+		rateCounter: rc.NewRateCounter(interval),
+		expvarInt:   expvar.NewInt(name),
+	}
+}
+
+// Incr increments the counter in an amount and saves exports
+// it to expvar
+func (c *ExpvarRateCounter) Incr(amount int64) {
+	c.rateCounter.Incr(amount)
+	c.expvarInt.Set(c.rateCounter.Rate())
+}
+
 // Pool holds a map of rate counters
 type Pool struct {
-	counters map[string]*rc.RateCounter
+	counters map[string]*ExpvarRateCounter
 	lock     sync.RWMutex
 }
 
 var pool = &Pool{
-	counters: make(map[string]*rc.RateCounter),
+	counters: make(map[string]*ExpvarRateCounter),
 }
 
 // FromPool returns a new counter from the pool identified by the prefix and interval
 // Example: Get a counter that will hold the counts per second to `/lbcheck`
 // endpoint:
 //   `ratecounters.Get("endpoints.lbcheck", 1 * time.Second)`
-func FromPool(prefix string, interval time.Duration) *rc.RateCounter {
-	key := fmt.Sprintf("%s::%s", prefix, interval.String())
-
+func FromPool(counterName string, interval time.Duration) *ExpvarRateCounter {
 	pool.lock.RLock()
-	counter, ok := pool.counters[key]
+	counter, ok := pool.counters[counterName]
 	pool.lock.RUnlock()
 
 	if !ok {
 		pool.lock.Lock()
-		pool.counters[key] = rc.NewRateCounter(interval)
+		pool.counters[counterName] = newExpvarRateCounter(counterName, interval)
 		pool.lock.Unlock()
 
 		pool.lock.RLock()
-		counter = pool.counters[key]
+		counter = pool.counters[counterName]
 		pool.lock.RUnlock()
 	}
+
+	if counter.interval != interval {
+		panic(fmt.Sprintf("Counter %s already exists with a different interval", counterName))
+	}
+
 	return counter
 }
--- go/ratecounter/pool_test.go
+++ go/ratecounter/pool_test.go
@@ -7,14 +7,28 @@ import (
 	test "github.com/outbrain/golib/tests"
 )
 
-func TestFromPool(t *testing.T) {
+func TestFromPoolKeepsASingleInstanceOfTheSameCounter(t *testing.T) {
 	counter := FromPool("myCounter", 1*time.Second)
 	anotherCounter := FromPool("myCounter", 1*time.Second)
 	aThirdCounter := FromPool("different", 1*time.Second)
-	aFourthCounter := FromPool("different", 2*time.Second)
 
-	test.S(t).ExpectEquals(len(pool.counters), 3)
+	test.S(t).ExpectEquals(len(pool.counters), 2)
 	test.S(t).ExpectEquals(counter, anotherCounter)
 	test.S(t).ExpectNotEquals(aThirdCounter, anotherCounter)
-	test.S(t).ExpectNotEquals(aThirdCounter, aFourthCounter)
+}
+
+func TestFromPoolPanicsIfSameCountersWithDifferentDuration(t *testing.T) {
+	defer func() {
+		recover()
+	}()
+	FromPool("myCounter", 1*time.Second)
+	FromPool("myCounter", 2*time.Second)
+	test.S(t).Errorf("Should have panic'ed")
+}
+
+func TestIncr(t *testing.T) {
+	counter := FromPool("myCounter", 1*time.Second)
+	counter.Incr(3)
+	test.S(t).ExpectEquals(counter.rateCounter.Rate(), int64(3))
+	test.S(t).ExpectEquals(counter.expvarInt.String(), "3")
 }
