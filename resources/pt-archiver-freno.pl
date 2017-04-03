303095aa8bd1b507de515bde0a6331c97b06e4d2
Shlomi Noach <shlomi-noach@github.com>
2017-04-03 12:41:39 +0300

    WIP: pt-archiver freno plugin

---
+++ resources/pt-archiver-freno.pl
@@ -0,0 +1,27 @@
+package Freno::Ptarchiver;
+
+use LWP::Simple;
+
+sub new {
+   my ( $class, %args ) = @_;
+   return bless(\%args, $class);
+}
+
+sub before_begin {}
+
+sub is_archivable {}
+
+sub before_delete {} # Take no action
+
+sub before_bulk_delete {
+  while(! head($freno_url)) {
+    select(undef, undef, undef, 0.25); # sleep
+  }
+  return 1;
+}
+
+sub before_insert {} # Take no action
+sub custom_sth    {} # Take no action
+sub after_finish  {} # Take no action
+
+1;
