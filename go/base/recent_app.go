3c5c9b7f038e9bbc66a2d686f070322f20f92620
Shlomi Noach <shlomi-noach@github.com>
2017-05-10 13:47:47 +0300

    CheckedAt is Unix epoch (seconds)

--- go/base/recent_app.go
+++ go/base/recent_app.go
@@ -7,13 +7,13 @@ import (
 // AppThrottle is the definition for an app throtting instruction
 // - Ratio: [0..1], 0 == no throttle, 1 == fully throttle
 type RecentApp struct {
-	CheckedAt           time.Time
+	CheckedAt           int64
 	MinutesSinceChecked int64
 }
 
 func NewRecentApp(checkedAt time.Time) *RecentApp {
 	result := &RecentApp{
-		CheckedAt:           checkedAt,
+		CheckedAt:           checkedAt.Unix(),
 		MinutesSinceChecked: int64(time.Since(checkedAt).Minutes()),
 	}
 	return result
