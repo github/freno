86ea65c5c4fcb55ae0cce50ede0956e9b889279e
Shlomi Noach <shlomi-noach@github.com>
2019-03-04 12:22:10 +0200

    refactored throttler hosts selection outside of ParseCvsHosts

---
+++ go/haproxy/throttle.go
@@ -0,0 +1,23 @@
+package haproxy
+
+import ()
+
+func FilterThrotllerHosts(backendHosts [](*BackendHost)) (hosts []string) {
+	for _, backendHost := range backendHosts {
+		hostIsRelevant := false
+		switch backendHost.Status {
+		case StatusUp:
+			if !backendHost.IsTransitioning {
+				hostIsRelevant = true
+			}
+		case StatusDown:
+			hostIsRelevant = true
+		case StatusNoCheck:
+			hostIsRelevant = true
+		}
+		if hostIsRelevant {
+			hosts = append(hosts, backendHost.Hostname)
+		}
+	}
+	return hosts
+}
