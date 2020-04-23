779c612d27422637969a49df188f7dc313dc7294
Tim Vaillancourt <timvaillancourt@github.com>
2020-04-23 13:51:04 +0200

    Use real http test

--- pkg/vitess/api_client_test.go
+++ pkg/vitess/api_client_test.go
@@ -1,39 +1,77 @@
 package vitess
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"vitess.io/vitess/go/vt/proto/topodata"
 )
 
-func TestFilterReplicaTablets(t *testing.T) {
-	replicas := filterReplicaTablets([]Tablet{
-		{
-			MysqlHostname: t.Name() + "1",
-			Type:          topodata.TabletType_MASTER,
-		},
-		{
-			MysqlHostname: t.Name() + "2", // this node is valid
-			Type:          topodata.TabletType_REPLICA,
-		},
-		{
-			MysqlHostname: t.Name() + "3",
-			Type:          topodata.TabletType_SPARE,
-		},
-		{
-			MysqlHostname: t.Name() + "4",
-			Type:          topodata.TabletType_BACKUP,
-		},
-		{
+func TestParseTablets(t *testing.T) {
+	vitessApi := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.String() {
+		case "/api/keyspace/test/tablets/00":
+			data, _ := json.Marshal([]Tablet{
+				{
+					MysqlHostname: "master",
+					Type:          topodata.TabletType_MASTER,
+				},
+				{
+					MysqlHostname: "replica",
+					Type:          topodata.TabletType_REPLICA,
+				},
+				{
+					MysqlHostname: "spare",
+					Type:          topodata.TabletType_SPARE,
+				},
+				{
+					MysqlHostname: "batch",
+					Type:          topodata.TabletType_BATCH,
+				},
+				{
+					MysqlHostname: "backup",
+					Type:          topodata.TabletType_BACKUP,
+				},
+				{
 
-			MysqlHostname: t.Name() + "5",
-			Type:          topodata.TabletType_RESTORE,
-		},
+					MysqlHostname: "restore",
+					Type:          topodata.TabletType_RESTORE,
+				},
+			})
+			fmt.Fprint(w, string(data))
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "[]")
+		}
+	}))
+	defer vitessApi.Close()
+
+	t.Run("not-found", func(t *testing.T) {
+		tablets, err := ParseTablets(vitessApi.URL, "not-found", "00")
+		if err != nil {
+			t.Fatalf("Expected no error, got %q", err)
+		}
+
+		if len(tablets) > 0 {
+			t.Fatalf("Expected 0 tablets, got %d", len(tablets))
+		}
 	})
-	if len(replicas) != 1 {
-		t.Fatalf("Expected 1 replica, got %v", replicas)
-	}
-	if replicas[0].MysqlHostname != t.Name()+"2" {
-		t.Fatalf("Expected hostname %q, got %q", t.Name()+"2", replicas[0].MysqlHostname)
-	}
+
+	t.Run("success", func(t *testing.T) {
+		tablets, err := ParseTablets(vitessApi.URL, "test", "00")
+		if err != nil {
+			t.Fatalf("Expected no error, got %q", err)
+		}
+
+		if len(tablets) != 1 {
+			t.Fatalf("Expected 1 tablet, got %d", len(tablets))
+		}
+
+		if tablets[0].MysqlHostname != "replica" {
+			t.Fatalf("Expected hostname %q, got %q", "replica", tablets[0].MysqlHostname)
+		}
+	})
 }
